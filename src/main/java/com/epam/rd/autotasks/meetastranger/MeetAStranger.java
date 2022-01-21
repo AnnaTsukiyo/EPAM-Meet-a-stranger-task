12f11690a6c6b8d6d8617fac82029d706669aee8
AnnaTsukiyo <annett.sav4enko@gmail.com>
2022-01-21 18:42:23 +0200

    Solved issue

--- src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
+++ src/main/java/com/epam/rd/autotasks/meetastranger/MeetAStranger.java
@@ -1,5 +1,7 @@
 package com.epam.rd.autotasks.meetastranger;
 
+import java.util.Scanner;
+
 public class MeetAStranger {
     public static void main(String[] args) {
         //Write a program, which read a String from System.in and print "Hello, <input string>"
